69e02f4c70c246792cb81530131ade87e7974796
SourceCodeWhen <jack.charles.burton@gmail.com>
2023-09-12 19:38:00 +0100

    Feat: entry finding code implemented for choosing bucket on hashmap

--- table.h
+++ table.h
@@ -17,6 +17,6 @@ typedef struct {
 
 void initTable(Table* table);
 void freeTable(Table* table);
-bool tableSet(Table* table, ObjString* key, Value value);
+bool tableSet(Table* table, struct ObjString* key, Value value);
 
 #endif
