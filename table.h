747ead9912e302751937b1c3a37e1383b3ac287c
SourceCodeWhen <jack.charles.burton@gmail.com>
2023-09-13 19:02:26 +0100

    Feat: ready to implement string interning

--- table.h
+++ table.h
@@ -21,5 +21,6 @@ bool tableGet(Table* table, struct ObjString* key, Value* value);
 bool tableSet(Table* table, struct ObjString* key, Value value);
 bool tableDelete(Table* table, ObjString* key);
 void tableAddAll(Table* from, Table* to);
+struct ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
 
 #endif
