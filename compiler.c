8459394f595b2725d46a548c498acd084e386c32
SourceCodeWhen <jack.charles.burton@gmail.com>
2023-09-17 15:03:01 +0100

    Fix: squashed edge case bug around shadowing assignments

--- compiler.c
+++ compiler.c
@@ -244,6 +244,9 @@ static int resolveLocal(Compiler* compiler, Token* name) {
     for (int i = compiler->localCount -1; i >= 0; i--) {
         Local* local = &compiler->locals[i];
         if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("can't read local variable in its own initializer.");
+            }
             return i;
         }
     }
@@ -259,7 +262,7 @@ static void addLocal(Token name) {
 
     Local* local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -290,8 +293,14 @@ static uint8_t parseVariable(const char* errorMessage) {
     return identifierConstant(&parser.previous);
 }
 
+static void markInitialized() {
+    current->locals[current->localCount - 1].depth =
+        current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialized();
         return;
     }
 
